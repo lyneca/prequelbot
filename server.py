0fd8e6f94324c9e2d4e16bd6efd426925072b8fb
Lyneca <atraguliauniliantauthr@gmail.com>
2018-03-23 14:47:49 +1100

    add missing return

--- server.py
+++ server.py
@@ -48,6 +48,7 @@ def random_top(user):
         r = requests.get('https://reddit.com/r/prequelmemes/top.json&sort=top&t=all', headers=reddit_headers).json()
     except JSONDecodeError:
         send_message(PAT, user, 'Sorry, there was a problem grabbing a quality meme from r/PrequelMemes.')
+        return
     post = random.choice(r['data']['children'])['data']
     message = "{} (/u/{}, {} points)".format(post['title'], post['author'], post['score'])
     send_message(PAT, user, message)
@@ -61,6 +62,7 @@ def random_new(user):
         r = requests.get('https://reddit.com/r/prequelmemes/new.json', headers=reddit_headers).json()
     except JSONDecodeError:
         send_message(PAT, user, 'Sorry, there was a problem grabbing a quality meme from r/PrequelMemes.')
+        return
     post = random.choice(r['data']['children'])['data']
     message = "{} (/u/{}, {} points)".format(post['title'], post['author'], post['score'])
     send_message(PAT, user, message)
@@ -74,6 +76,7 @@ def newest(user):
         r = requests.get('https://reddit.com/r/prequelmemes/new.json', headers=reddit_headers).json()
     except JSONDecodeError:
         send_message(PAT, user, 'Sorry, there was a problem grabbing a quality meme from r/PrequelMemes.')
+        return
     post = r['data']['children'][0]['data']
     message = "{} (/u/{}, {} points)".format(post['title'], post['author'], post['score'])
     send_message(PAT, user, message)
